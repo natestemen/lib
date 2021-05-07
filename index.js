00d45fb2b9b989273397faaa09f1fea6c369acf7
nate stemen <nate@stemen.email>
2021-05-07 10:11:55 -0400

    got the basic display working

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require("express");
+const Pool = require("pg").Pool;
+const pool = new Pool({
+  user: "nate",
+  host: "/var/run/postgresql",
+  database: "lib",
+  password: "secure",
+  port: 5432,
+});
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  pool.query("SELECT title, author, fiction FROM books", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.render("index", { title: "books", books: results.rows });
+  });
+});
+
+module.exports = router;
