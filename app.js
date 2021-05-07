9d6add6a74328aff4b3aedffdf44c49c6631e9e3
nate stemen <nate@stemen.email>
2021-05-07 15:07:15 -0400

    add new book functionality

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var router = require("./index");
+var router = require("./router");
 
 var app = express();
 
@@ -14,7 +14,7 @@ app.set("view engine", "pug");
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
---
+++ router.js
@@ -0,0 +1,44 @@
+var express = require("express");
+const { Pool } = require("pg");
+const pool = new Pool({
+  user: "nate",
+  host: "/var/run/postgresql",
+  database: "lib",
+  password: "secure",
+  port: 5432,
+});
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  pool.query(
+    "SELECT title, author, fiction FROM books ORDER BY title",
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.render("index", { title: "books", books: results.rows });
+    }
+  );
+});
+
+router.get("/new", function (request, response, next) {
+  response.render("new");
+});
+
+router.post("/new", function (request, response, next) {
+  let newBook = [request.body.title, request.body.author, request.body.fiction];
+  pool.query(
+    "INSERT INTO books(title, author, fiction) VALUES ($1, $2, $3)",
+    newBook, // NEEDS SANITIZATION
+    (error, response) => {
+      if (error) {
+        throw error;
+      }
+      console.log(response);
+    }
+  );
+  response.status(200).json({ hello: request.body.title });
+});
+
+module.exports = router;
